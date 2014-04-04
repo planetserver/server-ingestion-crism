e8275d059cdb2943448e83b8359db220597c3ee4
Jelmer Oosthoek <jhpoosthoek@gmail.com>
2014-04-04 19:33:15 +0200

    minor update

--- addcrismmetadata.py
+++ addcrismmetadata.py
@@ -22,17 +22,18 @@ for line in f:
     collname = line[1]
     print "Adding: " + collname
     #meta = open(os.path.join(os.path.dirname, collname + ".js","r").readline().strip()
-    meta = '''&lt;gmlcov:Extension&gt;&lt;PlanetServerPDSMetadata xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; xlink:href=&quot;http://oderest.rsl.wustl.edu/live/?query=product&amp;results=m&amp;output=XML&amp;pdsid=%s&quot; xlink:type=&quot;simple&quot;/&gt;&lt;/gmlcov:Extension&gt;''' % (collname[:-5].lower())
-    #meta = '''<gmlcov:Extension><PlanetServerPDSMetadata xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; xlink:href=&quot;http://oderest.rsl.wustl.edu/live/?query=product&amp;results=m&amp;output=XML&amp;pdsid=%s&quot; xlink:type=&quot;simple&quot;/></gmlcov:Extension>''' % (collname[:-5].lower())
+    #meta = '''&lt;gmlcov:Extension&gt;&lt;PlanetServerPDSMetadata xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot; xlink:href=&quot;http://oderest.rsl.wustl.edu/live/?query=product&amp;results=m&amp;output=XML&amp;pdsid=%s&quot; xlink:type=&quot;simple&quot;/&gt;&lt;/gmlcov:Extension&gt;''' % (collname[:-5].lower())
+    meta = '<gmlcov:Extension><PlanetServerPDSMetadata xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="http://oderest.rsl.wustl.edu/live/?query=product&amp;results=m&amp;output=XML&amp;pdsid=%s" xlink:type="simple"/></gmlcov:Extension>' % (collname[:-5].lower())
+    meta = meta.replace('"', '\\"')
     # delete before add
     psql.do('''DELETE FROM ps_extra_metadata WHERE coverage_id=(SELECT id FROM ps_coverage WHERE name=\x27%s\x27);''' % (collname))
     # slice up in pieces
     #splitmeta = [meta[i:i+max] for i in range(0, len(meta), max)]
     # insert first piece
     psql.do('''INSERT INTO ps_extra_metadata (coverage_id, metadata_type_id, value) VALUES (
-               (SELECT id FROM ps_coverage WHERE name = \x27%s\x27), 
-               (SELECT id FROM ps_extra_metadata_type WHERE type='gmlcov'),
-               \x27%s\x27);''' % (collname, meta))
+               (SELECT id FROM ps_coverage WHERE name = \'%s\'), 
+               (SELECT id FROM ps_extra_metadata_type WHERE type=\'gmlcov\'),
+               \'%s\');''' % (collname, meta))
     #psql.do("INSERT INTO ps_metadata (coverage, metadata) VALUES ((SELECT id FROM ps_coverage WHERE name=\x27%s\x27), \x27%s\x27);" % (collname, meta)) #splitmeta[0]))
     # append rest of pieces
     # for string in splitmeta[1:]:
--- rasimporter.py
+++ rasimporter.py
@@ -1,4 +1,4 @@
-from osgeo import gdal
+from osgeo import gdal, osr
 from osgeo.gdalconst import *
 import os,sys
 import subprocess
@@ -50,20 +50,31 @@ for item in filecoll:
     if not coll_name in ingested:
         # gdal
         inDs = gdal.Open(file_to_insert, GA_ReadOnly)
-
+        
+        # Detect CRS:
+        prj = inDs.GetProjection()
+        srs = osr.SpatialReference(wkt=prj)
+        prj4 = str(srs.ExportToProj4())
+        if prj4 == "+proj=longlat +a=3396190 +b=3396190 +no_defs ":
+            crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/1/ --crs-order 1:0'
+        elif prj4 == "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=3396190 +b=3396190 +units=m +no_defs ":
+            crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
+        else:
+            print "Wrong CRS! " + prj4
+            sys.exit()
+        
+        print "PlanetServer CRS: " + crs
+        
         if inDs.RasterCount == 3:
             # assuming 3 x 8-bit rasters
             datatype = "RGBImage:RGBSet"
         elif inDs.RasterCount == 1:
             if inDs.GetRasterBand(1).DataType == 1:
                 datatype = "GreyImage:GreySet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             elif inDs.GetRasterBand(1).DataType == 3:
                 datatype = "ShortImage:ShortSet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             elif inDs.GetRasterBand(1).DataType == 6:
                 datatype = "FloatImage:FloatSet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             else:
                 print "Unsupported data type: " + str(inDs.GetRasterBand(1).DataType)
                 print "Please use rasset.py to add data type"
@@ -71,13 +82,10 @@ for item in filecoll:
         else:
             if inDs.RasterCount == 107:
                 datatype = "CRISMVNIRImage:CRISMVNIRSet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             elif inDs.RasterCount == 438:
                 datatype = "CRISMIRImage:CRISMIRSet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             elif inDs.RasterCount == 72:
                 datatype = "CRISMMRDRImage:CRISMMRDRSet"
-                crs = 'http://planetserver.jacobs-university.de:8080/def/crs/PS/0/2/'
             else:
                 print "Unsupported data type, please use rasset.py to add data type"
                 sys.exit()
@@ -87,11 +95,11 @@ for item in filecoll:
         command = "/home/earthserver/rc/applications/rasgeo/rasimport -f %s -t %s --coll %s --coverage-name %s --crs-uri %s --conn /home/earthserver/.rasdaman/rasconnect" % (file_to_insert, datatype, coll_name, coll_name, crs)
         # >/dev/null
         output = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
-        #os.system(command)
         print output
         if "ERROR" in output:
             break
         time.sleep(3)
+        #print command
         
         #if "ERROR - rimport::main, l. 1467: RasManager Error: Write transaction in progress, please retry again later." in output or "ERROR - rimport::main, l. 1467: RasManager Error: System overloaded, please try again later" in output:
         #    print "Please wait a few minutes and start again"
